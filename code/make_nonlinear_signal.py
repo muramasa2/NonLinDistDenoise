ca62eef4e922121f05e20e251e1c8c3f2fdb87f3
muramasa2 <4318571@ed.tus.ac.jp>
2019-06-24 23:24:23 +0900

    任意の高調波を含んだ歪み波形の作成プログラム追加

---
+++ code/make_nonlinear_signal.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+from glob import glob
+from scipy.fftpack import fft, ifft
+from scipy import hanning
+
+################
+# def function #
+################
+def signal_fft(signal, N, win):
+
+
+    # フーリエ変換
+    spectrum = fft(signal*win) # 窓関数あり
+    spectrum_abs = np.abs(spectrum) # 振幅を元に信号に揃える
+    half_spectrum = spectrum_abs[:N//2+1] / (signal.shape[0] / 2)
+    half_spectrum[0] = half_spectrum[0] / 2 # 直流成分（今回は扱わないけど）は2倍不要
+    half_spectrum_dBV = 20*np.log10(half_spectrum)
+
+    return spectrum, half_spectrum_dBV
+
+
+####################
+# make signal data #
+####################
+fs = 44100
+L = 60
+f = 1019
+
+t = np.arange(0,L,1/44100)
+amp = np.sqrt(2)*1 # 1Vrms = √2Vpp
+base_signal = amp * np.sin(2*np.pi*f*t)
+
+num = 10 #何次高調波まで見るか
+
+mode = 'even'
+
+if mode == 'all':
+    start = 2
+    step = 1
+    save_path = './figure/make_all_non_lineardist_signal.jpg'
+else:
+    step = 2
+    if mode == 'even':
+        start = 2
+        save_path = './figure/make_even_non_lineardist_signal.jpg'
+    elif mode == 'odd':
+        start = 3
+        save_path = './figure/make_odd_non_lineardist_signal.jpg'
+
+non_lin_dist = [( 4.472 * 10**(-6)/ 2**(i-1)) * np.sin(2*np.pi*(i)*f*t) for i in range(start,num+1,step)]
+dist_signal = base_signal+sum(non_lin_dist)
+
+##############
+# FFT & IFFT #
+##############
+N = 1024000 #フーリエ変換長[サンプル]
+L_reduce = N/fs #フーリエ音源長[s] = N/fs
+
+# 窓関数
+win = hanning(N)
+
+signal_reduce = base_signal[:N] #音源長を60s＝2545000サンプルに固定
+spectrum, half_spectrum_dBV = signal_fft(signal_reduce, N, win)
+
+# # フーリエ逆変換
+# resyn_sig = ifft(spectrum) * ((signal_reduce.shape[0])/2)
+# resyn_sig /= win
+
+dist_signal_reduce = dist_signal[:N] #音源長を60s＝2545000サンプルに固定
+dist_spectrum, dist_half_spectrum_dBV = signal_fft(dist_signal_reduce, N, win)
+
+# フーリエ逆変換
+# resyn_sig = ifft(spectrum) * ((dist_signal_reduce.shape[0])/2)
+# resyn_sig /= win
+
+#############
+# plot data #
+#############
+t_reduce = np.arange(0,L_reduce,L_reduce/N) #横軸時間軸[s]
+f = np.arange(0,fs//2,(fs//2)/half_spectrum_dBV.shape[0]) #横軸周波数軸[Hz]
+
+# 図を表示
+fig, ax = plt.subplots(1,2,figsize=(15,5))
+fig.subplots_adjust(wspace=0.2,hspace=0.2)
+ax[0].plot(t_reduce, dist_signal_reduce, 'r')
+ax[0].plot(t_reduce, signal_reduce, 'b')
+ax[0].legend(['distoted','not distorted'], loc='upper right')
+ax[0].set_xlim(0,0.01)
+ax[0].set_xlabel('time[s]',fontsize=18)
+ax[0].set_ylabel('amplitude[V]',fontsize=18)
+
+ax[1].semilogx(f, dist_half_spectrum_dBV, 'r')
+ax[1].semilogx(f, half_spectrum_dBV, 'b')
+ax[1].legend(['distoted','not distorted'], loc='upper right')
+ax[1].set_xlim(20,22500)
+ax[1].set_xlabel('freqency[Hz]',fontsize=18)
+ax[1].set_ylabel('amplitude[dBV]',fontsize=18)
+
+plt.show()
+fig.savefig(save_path)
--- code/preprocess.py
+++ code/preprocess.py
@@ -3,7 +3,21 @@ import matplotlib.pyplot as plt
 import soundfile as sf
 from glob import glob
 from scipy.fftpack import fft, ifft
-from scipy import hamming
+from scipy import hamming, hanning
+
+
+def signal_fft(signal, N):
+    # 窓関数
+    win = hanning(N)
+
+    # フーリエ変換
+    spectrum = fft(signal*win) # 窓関数あり
+    spectrum_abs = np.abs(spectrum) # 振幅を元に信号に揃える
+    half_spectrum = spectrum_abs[:N//2+1] / (signal.shape[0] / 2)
+    half_spectrum[0] = half_spectrum[0] / 2 # 直流成分（今回は扱わないけど）は2倍不要
+    half_spectrum_dBV = 20*np.log10(half_spectrum)
+
+    return spectrum, half_spectrum_dBV
 
 #################
 # load wav data #
@@ -13,45 +27,66 @@ dist_wav_path = glob('./result_wave/*.wav')
 
 for i in range(len(origin_wav_path)):
     origin_wav, fs = sf.read(origin_wav_path[i])
-    # dist_wav, _ = sf.read(dist_wav_path[i])
+    dist_wav, fs = sf.read(dist_wav_path[i])
 
     N = 102400 #フーリエ変換長[サンプル]
     L = N/fs #音源長[s] = N/fs
 
     origin_wav = origin_wav[:N] #音源長を60s＝2545000サンプルに固定
-    # dist_wav = dist_wav[:N]
+    dist_wav = dist_wav[:N]
 
-    # 窓関数
-    win = hamming(N)
+    spectrum, half_spectrum_dBV = signal_fft(origin_wav, N)
+    dist_spectrum, dist_half_spectrum_dBV = signal_fft(dist_wav, N)
 
-    # フーリエ変換
-    spectrum = fft(origin_wav * win)/ ((origin_wav.shape[0])/2) # 窓関数あり
-    half_spectrum = abs(spectrum[:N//2+1])
-    half_spectrum_dBV = 20*np.log10(half_spectrum)
+    # print(max(origin_wav), max(dist_wav))
+    # print(max(half_spectrum_dBV),max(dist_half_spectrum_dBV))
+    # print(half_spectrum_dBV)
+
+    # print(origin_wav, dist_wav)
+    # print(half_spectrum_dBV,dist_half_spectrum_dBV)
 
     # フーリエ逆変換
-    resyn_sig = ifft(spectrum) * ((origin_wav.shape[0])/2)
-    resyn_sig /= win
+    # resyn_sig = ifft(spectrum) * ((origin_wav.shape[0])/2)
+    # resyn_sig /= win
 
     t = np.arange(0,L,L/N) #横軸時間軸[s]
-    f = np.arange(0,fs//2,(fs//2)/half_spectrum.shape[0]) #横軸周波数軸[Hz]
+    f = np.arange(0,fs//2,(fs//2)/half_spectrum_dBV.shape[0]) #横軸周波数軸[Hz]
+
     # 図を表示
     if i == 0:
-        fig, ax = plt.subplots(len(origin_wav_path),3,figsize=(40,40))
+        fig, ax = plt.subplots(len(origin_wav_path),2,figsize=(40,40))
         fig.subplots_adjust(wspace=0.2,hspace=0.2)
-    ax[i,0].plot(t,origin_wav)
+    # ax[i,0].plot(t,origin_wav)
+    # ax[i,0].set_xlim(0,0.01)
+    # ax[i,0].set_xlabel('time[s]',fontsize=18)
+    # ax[i,0].set_ylabel('amplitude[V]',fontsize=18)
+    #
+    # ax[i,1].semilogx(f,dist_half_spectrum_dBV)
+    # ax[i,1].set_xlim(20,22500)
+    # ax[i,1].set_xlabel('freqency[Hz]',fontsize=18)
+    # ax[i,1].set_ylabel('amplitude[dBV]',fontsize=18)
+    #
+    # ax[i,2].plot(t,resyn_sig)
+    # ax[i,2].set_xlim(0,0.01)
+    # ax[i,2].set_xlabel('time[s]',fontsize=18)
+    # ax[i,2].set_ylabel('amplitude[V]',fontsize=18)
+
+    ax[i,0].plot(t,origin_wav,t,dist_wav)
+    ax[i,0].legend(['origin','TEAC'])
     ax[i,0].set_xlim(0,0.01)
     ax[i,0].set_xlabel('time[s]',fontsize=18)
     ax[i,0].set_ylabel('amplitude[V]',fontsize=18)
 
-    ax[i,1].semilogx(f,half_spectrum_dBV)
+    # ax[i,0].semilogx(f,half_spectrum_dBV)
+    # ax[i,0].set_xlim(20,22500)
+    # ax[i,0].set_xlabel('freqency[Hz]',fontsize=18)
+    # ax[i,0].set_ylabel('amplitude[dBV]',fontsize=18)
+
+    ax[i,1].semilogx(f,half_spectrum_dBV,f,dist_half_spectrum_dBV)
+    ax[i,1].legend(['origin','TEAC'])
     ax[i,1].set_xlim(20,22500)
     ax[i,1].set_xlabel('freqency[Hz]',fontsize=18)
     ax[i,1].set_ylabel('amplitude[dBV]',fontsize=18)
 
-    ax[i,2].plot(t,resyn_sig)
-    ax[i,2].set_xlim(0,0.01)
-    ax[i,2].set_xlabel('time[s]',fontsize=18)
-    ax[i,2].set_ylabel('amplitude[V]',fontsize=18)
 plt.show()
-fig.savefig('input_wave.jpg')
+fig.savefig('./figure/origin_dist_input_wave.jpg')
